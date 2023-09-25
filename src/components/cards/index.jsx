1702f71a3563070ba95fc33ff76c15c74a0b37e9
Haris <haris.usman@hotmail.com>
2023-09-25 13:00:32 +0200

    Added POST functionality

--- src/components/cards/index.jsx
+++ src/components/cards/index.jsx
@@ -3,14 +3,18 @@ import useGetData from "../../hooks/api/getData";
 import { fetchUrl } from "../../utils/constants";
 import { PrimaryButton, SecondaryButton } from "../commonStyles/buttons";
 import { Link } from "react-router-dom";
+import heic2any from "heic2any";
+import { v4 as uuidv4 } from "uuid";
+import { save } from "../../hooks/storage";
+
+function getSingleAdventureUrl(adventureId) {
+  return `https://k0wle2wy.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%22adventures%22+%26%26+_id+%3D%3D+%22${adventureId}%22%5D%7B%0A++_id%2C%0A++title%2C%0A++year%2C%0A++images%5B%5D%7B%0A++++%22imageUrl%22%3A+image.asset-%3Eurl%2C%0A++++caption%0A++%7D%0A%7D%0A%0A%0A`;
+}
 
 export default function AdventureCards() {
   // REMOVED ISLOADING AND ISERROR(RE-ADD THEM)
-
   const { data } = useGetData(fetchUrl);
-
   const sortedData = data.sort((a, b) => a.year - b.year);
-
   const [collapsed, setCollapsed] = useState({});
 
   const toggleCollapse = (id) => {
@@ -18,6 +22,129 @@ export default function AdventureCards() {
       ...prevState,
       [id]: !prevState[id],
     }));
+  };
+
+  const handleUpload = async (fileToUpload) => {
+    const projectId = "k0wle2wy";
+    const dataset = "production";
+    const apiVersion = "v2021-10-21";
+    const token =
+      "skQXBZxoC3J1ch9qK2xi7tE7h3rfUm7dt0OvXDUmsJmEULZO7mg3kQVna8Itj22FSzFGr5rEqBygZfRcO9NdvvNLRUFhQgivM3bcvbkEUJbH0HOcs9gECvR8WBaRh1suom3zp5WKAB5VCkO7HWRM0aTmzH5zsWkZRntVq5GhO04Z31KEEJFl";
+
+    const arrayBuffer = await fileToUpload.arrayBuffer();
+    const byteArray = new Uint8Array(arrayBuffer);
+
+    const uploadUrl = `https://${projectId}.api.sanity.io/${apiVersion}/assets/images/${dataset}`;
+    const uploadRes = await fetch(uploadUrl, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": fileToUpload.type,
+      },
+      body: byteArray,
+    });
+
+    if (uploadRes.ok) {
+      const uploadData = await uploadRes.json();
+      return uploadData.document._id;
+    } else {
+      console.error("Failed to upload image");
+      return null;
+    }
+  };
+
+  const updateAdventureWithImage = async (adventureId, imageId) => {
+    const projectId = "k0wle2wy";
+    const dataset = "production";
+    const apiVersion = "v2021-10-21";
+    const token =
+      "skQXBZxoC3J1ch9qK2xi7tE7h3rfUm7dt0OvXDUmsJmEULZO7mg3kQVna8Itj22FSzFGr5rEqBygZfRcO9NdvvNLRUFhQgivM3bcvbkEUJbH0HOcs9gECvR8WBaRh1suom3zp5WKAB5VCkO7HWRM0aTmzH5zsWkZRntVq5GhO04Z31KEEJFl";
+
+    // Fetch the current adventure document
+    const getUrl = `https://${projectId}.api.sanity.io/${apiVersion}/data/query/${dataset}?query=*[_id=="${adventureId}"][0]`;
+    const getRes = await fetch(getUrl, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!getRes.ok) {
+      console.error("Failed to fetch adventure");
+      return;
+    }
+
+    const adventure = await getRes.json();
+    const currentImages = adventure.result.images || [];
+
+    const imageWithCaption = {
+      _type: "imageWithCaption",
+      _key: uuidv4(),
+      image: {
+        _type: "image",
+        _key: uuidv4(),
+        asset: {
+          _type: "reference",
+          _ref: imageId,
+        },
+      },
+      caption: "", // You can set a default caption or leave it empty
+    };
+
+    currentImages.push(imageWithCaption);
+
+    const postUrl = `https://${projectId}.api.sanity.io/${apiVersion}/data/mutate/${dataset}`;
+    const postRes = await fetch(postUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        mutations: [
+          {
+            patch: {
+              id: adventureId,
+              set: {
+                images: currentImages,
+              },
+            },
+          },
+        ],
+      }),
+    });
+
+    if (postRes.ok) {
+      console.log("Image added to adventure successfully");
+    } else {
+      console.error("Failed to add image to adventure");
+    }
+  };
+
+  const handleFileChange = async (event, adventureId) => {
+    console.log("Adventure ID:", adventureId);
+    const file = event.target.files[0];
+
+    let assetId;
+    if (file.type === "image/heic") {
+      const convertedBlob = await heic2any({
+        blob: file,
+        toType: "image/jpeg",
+        quality: 1,
+      });
+      assetId = await handleUpload(convertedBlob);
+    } else {
+      assetId = await handleUpload(file);
+    }
+    if (assetId) {
+      await updateAdventureWithImage(adventureId, assetId);
+      const response = await fetch(getSingleAdventureUrl(adventureId));
+      const updatedAdventureData = await response.json();
+
+      // Extract the result field from the response
+      const updatedAdventure = updatedAdventureData.result;
+
+      save(getSingleAdventureUrl(adventureId), updatedAdventure);
+    }
   };
 
   return (
@@ -54,7 +181,22 @@ export default function AdventureCards() {
             } w-100 col-sm-12 d-flex justify-content-center`}
           >
             <div className="w-100 mx-2 mb-2">
-              <SecondaryButton className="">LEGG TIL</SecondaryButton>
+              <SecondaryButton className="">
+                <label
+                  className="cursor-pointer"
+                  htmlFor={`icon-button-file-${adventure._id}`}
+                >
+                  LEGG TIL
+                </label>
+                <input
+                  accept="image/jpeg, image/png, image/heic, image/heif"
+                  id={`icon-button-file-${adventure._id}`}
+                  className="d-none"
+                  type="file"
+                  capture="environment"
+                  onChange={(e) => handleFileChange(e, adventure._id)}
+                />
+              </SecondaryButton>
             </div>
             <Link
               className="mx-2 mb-2 w-100"
